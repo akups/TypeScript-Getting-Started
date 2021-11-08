2f0f0f0100ece446046dcf32d1b37dc6c2526ed2
Akua <akua@candis.io>
2021-11-08 14:06:16 +0100

    creating a utility class

--- js/app.js
+++ js/app.js
@@ -16,15 +16,6 @@ function logPlayer(name) {
     if (name === void 0) { name = 'MultiMath Player'; }
     console.log("New game starting for player:" + name);
 }
-function getInputValue(elementID) {
-    var inputElement = document.getElementById(elementID);
-    if (inputElement.value === '') {
-        return undefined;
-    }
-    else {
-        return inputElement.value;
-    }
-}
 function postScore(score, playerName) {
     if (playerName === void 0) { playerName = 'MultiMath Player'; }
     var logger;
@@ -46,4 +37,35 @@ function logError(err) {
 var firstPlayer = new Player;
 firstPlayer.name = 'Maggie';
 console.log(firstPlayer.formatName());
+var Scoreboard = (function () {
+    function Scoreboard() {
+        this.results = [];
+    }
+    Scoreboard.prototype.addResult = function (newResult) {
+        this.results.push(newResult);
+    };
+    Scoreboard.prototype.updateSocreboard = function () {
+        var output = "<h2>Scoreboard</h2>";
+        for (var index = 0; index < this.results.length; index++) {
+            var result = this.results[index];
+            output += "</h4>";
+            output += result.playerName + " : " + result.score + " / " + result.problemCount + " for factor " + result.factor;
+            output += "</h4>";
+        }
+        var scoreElement = document.getElementById('scores');
+        scoreElement.innerHTML = output;
+    };
+    return Scoreboard;
+}());
+var Utility = (function () {
+    function Utility() {
+    }
+    Utility.getInputValue = function (elementID) {
+        var inputElement = document.getElementById(elementID);
+        {
+            return inputElement.value;
+        }
+    };
+    return Utility;
+}());
 //# sourceMappingURL=app.js.map
